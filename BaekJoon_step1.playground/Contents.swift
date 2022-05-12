27f0ea4e41c5222c3abf9ae12cafb90ec63e973f
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-05-12 20:06:14 +0900

    step1 입출력과 사칙연산

---
+++ BaekJoon_step1.playground/Contents.swift
@@ -0,0 +1,79 @@
+// 입출력과 사칙연산
+import Foundation
+
+//2557
+print("Hello World!", terminator: "")
+
+//10718
+print("강한친구 대한육군", "강한친구 대한육군", separator: "\n", terminator: "")
+
+//10171
+print(#"""
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+"""#)
+
+//25083
+print(#"""
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+"""#)
+/*
+//1000
+let inputString = readLine()!
+let inputStringArray = inputString.components(separatedBy: " ")
+print(Int(inputStringArray[0])! + Int(inputStringArray[1])!)
+
+//10998
+let inputString = readLine()!
+let inputStringArray = inputString.components(separatedBy: " ")
+print(Int(inputStringArray[0])! * Int(inputStringArray[1])!)
+
+//1008
+let inputString = readLine()!
+let inputStringArray = inputString.components(separatedBy: " ")
+print(Double(inputStringArray[0])! / Double(inputStringArray[1])!)
+ 
+//10869
+let inputString = readLine()!
+let inputStringArray = inputString.components(separatedBy: " ")
+print(Int(inputStringArray[0])! + Int(inputStringArray[1])!)
+print(Int(inputStringArray[0])! - Int(inputStringArray[1])!)
+print(Int(inputStringArray[0])! * Int(inputStringArray[1])!)
+print(Int(inputStringArray[0])! / Int(inputStringArray[1])!)
+print(Int(inputStringArray[0])! % Int(inputStringArray[1])!)
+ 
+//10926
+let inputString = readLine()!
+print(inputString+"??!")
+ 
+//18108
+let inputString = readLine()!
+print(Int(inputString)! - 543)
+
+//10430
+let inputString = readLine()!
+let inputStringArray = inputString.components(separatedBy: " ")
+
+print((Int(inputStringArray[0])!+Int(inputStringArray[1])!)%Int(inputStringArray[2])!)
+print(((Int(inputStringArray[0])!%Int(inputStringArray[2])!) + (Int(inputStringArray[1])!%Int(inputStringArray[2])!))%Int(inputStringArray[2])!)
+
+print((Int(inputStringArray[0])!*Int(inputStringArray[1])!)%Int(inputStringArray[2])!)
+print(((Int(inputStringArray[0])!%Int(inputStringArray[2])!) * (Int(inputStringArray[1])!%Int(inputStringArray[2])!))%Int(inputStringArray[2])!)
+
+//2588
+let a = Int(readLine()!)!
+let b = Int(readLine()!)!
+
+print(a * (b%10))
+print(a * ((b/10)%10))
+print(a * (b/100))
+print(a * (b/100) * 100 + a * ((b/10)%10) * 10 + a * (b%10))
+
+ */
